d72b39bcd18edac38f79894b7836d394f7ea8506
progrrah <mojang883@gmail.com>
2024-03-15 22:37:45 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/raxim4uk/Projects/new
+# Build directory: /home/raxim4uk/Projects/new/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
